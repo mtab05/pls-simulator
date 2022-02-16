bddb9639a53ef366301eb55c286f15cafb8e9998
Reyad ElMahdy <62578725+meyadelrahdy@users.noreply.github.com>
2022-02-16 11:47:41 -0500

    Update

--- sketches-to-test-parts/Python/ActuatorFSM.py
+++ sketches-to-test-parts/Python/ActuatorFSM.py
@@ -1,8 +1,8 @@
+from multiprocessing.connection import wait
 from numpy import mean
 
-
-global state
-state = [0,0,0,0]
+# bound = 40
+state = [0,0,0,0,0]
 def windowData(line):
     tstamps = win_trans = win_pitch = win_yaw = win_roll = []
     for i in range(0,5,len(line)):
@@ -20,10 +20,47 @@ def getDir(win):
         return 0
 
 def actuatorFSM(tstamps,trans,pitch,yaw,roll):
+    global state
     tsign = getDir(trans)
     psign = getDir(pitch)
     ysign = getDir(yaw)
     rsign = getDir(roll)
-    match state:
-        case [0,0,0,0]:
-            state = []
+    if mean(trans) > tbound:
+        state[0] = state[0] + 1
+    else:
+        state[0] = state[0] - 1
+        if (trans[-1]-trans(0))/(tstamps[-1]-tstamps[0]) > vthresh:
+            state[1] = state[1] + 1
+        else:
+            state[0] = state[0] - 1
+            if mean(pitch) > pbound or mean(yaw) > ybound:
+                if mean(pitch) > pbound:
+                    state[2] = state[2] + 1
+                else:
+                    state[2] = state[2] - 1
+                if mean(yaw) > ybound:
+                    state[3] = state[3] + 1
+                else:
+                    state[3] = state[3] - 1
+            else: 
+                if mean(roll) > rbound:
+                    state[4] = state[4] + 1
+                else:
+                    state[4] = state[4] - 1
+    match state.index:
+        case 0:
+            withdraw_sr(tsign)
+            state[0] = 0
+        case 1:
+            accel_sr(tsign)
+            state[1] = 0
+        case 2:
+            adjust_pitch(psign)
+            state[2] = 0
+        case 3:
+            adjust_yaw(ysign)
+            state[3] = 0
+        case 4: 
+            adjust_roll(rsign)
+            state[4] = 0
+    wait(2000)
